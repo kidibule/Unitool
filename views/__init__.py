753ca0b274de31839e63185809fbf5d872c09599
kidibule <tropicojeux@hotmail.com>
2026-04-10 11:16:26 +0200

    init to view

---
+++ views/__init__.py
@@ -0,0 +1,19 @@
+"""Package views — composants UI de l'application."""
+
+from .main_view import MainView
+from .scanner_frame import ScannerFrame
+from .contract_frame import ContractFrame
+from .logger_frame import LoggerFrame
+from .intelligence_frame import IntelligenceFrame
+from .interception_frame import InterceptionFrame
+from .ship_frame import ShipFrame
+
+__all__ = [
+    "MainView",
+    "ScannerFrame",
+    "ContractFrame",
+    "LoggerFrame",
+    "IntelligenceFrame",
+    "InterceptionFrame",
+    "ShipFrame",
+]
